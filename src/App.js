9d41e761e71e06ed413999f48e306d29ece69ba5
Daniel Merrill <daniel.r.merrill@gmail.com>
2018-01-16 15:40:30 -0800

    Add Monaco and Panels
    Add Monaco and Panels

--- src/App.js
+++ src/App.js
@@ -2,32 +2,88 @@ import React, { Component } from 'react';
 
 import BigNumber from 'bignumber.js'
 import CryptoJS from 'crypto-js'
-import Codemirror from 'react-codemirror'
+
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+import { Dropdown, IDropdown, DropdownMenuItemType, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+import { Nav, INavProps } from 'office-ui-fabric-react/lib/Nav';
+import { DefaultButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+
+import PanelGroup from 'react-panelgroup'
+import MonacoEditor from 'react-monaco-editor';
+import Consoled from 'react-consoled';
+
 import Web3 from 'web3'
 
 import './App.css';
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import 'codemirror/mode/htmlmixed/htmlmixed'
 
 import { gethAddress, swarmAddress, rootAddress } from './constants/api'
 
 const defaults = {
-  html: '<!-- HTML -->',
-  js: '// JS',
-  css : '/* CSS */',
+  html: {
+    label: 'HTML',
+    value: '<!-- HTML -->',
+    visible: true,
+  },
+  css: {
+    label: 'CSS',
+    value: '/* CSS */',
+    visible: false,
+  },
+  js: {
+    label: 'JS',
+    value: '// JS',
+    visible: true,
+  },
+  console: {
+    label: 'Console',
+    value: '[ console ]',
+    visible: true,
+  },
+  output: {
+    label: 'Output',
+    visible: false,
+  },
 }
+
+const codeEditors = ['html', 'css', 'js']
+const panelOrder = ['html', 'css', 'js', 'console', 'output']
+
+const DIVIDER_WIDTH = 1
 
 const key = 'hR&$yc=oJHXRN?Yo/^fqPtShjnXPF4ehd1?!O}6t#{jnzq7MsWlDX,,bfh_bvX_'
 
 class App extends Component {
   state = {
+    consoleJs: 'console.log("[ console ]")',
+    css: defaults.css.value,
+    html: defaults.html.value,
+    js: defaults.js.value,
     loading: false,
     readOnly: false,
-    html: defaults.html,
-    css: defaults.css,
-    js: defaults.js,
+    panels: {
+      html: {
+        size: 100,
+        visible: defaults.html.visible,
+      },
+      css: {
+        size: 100,
+        visible: defaults.css.visible,
+      },
+      js: {
+        size: 100,
+        visible: defaults.js.visible,
+      },
+      console: {
+        size: 100,
+        visible: defaults.console.visible,
+      },
+      output: {
+        size: 100,
+        visible: defaults.output.visible,
+      },
+    },
+    panelWidths: [],
   }
 
   componentDidMount() {
@@ -40,37 +96,28 @@ class App extends Component {
     if (window.location.pathname.split( '/' )[1]) {
       this.getData(window.location.pathname.split( '/' )[1])
     }
+    this.distributePanelsEvenly()
   }
 
   compile = () => {
-    const iframe = document.getElementById('code')
+    this.setState(state => ({ consoleJs: state.js }))
+    const iframe = this._iframe
+    if (!iframe) return
     const code = iframe.contentWindow.document;
 
     code.open();
-    code.writeln(this._htmlTextArea.textareaNode.innerText)
+    code.writeln(this.state.html)
 
     // Set web3 so it can be used with `console.log()` in js snippets
     code.writeln(`<script>window.web3 = window.parent.web3</script>`);
     code.writeln(`<script>window.BigNumber = window.parent.BigNumber</script>`);
-    code.writeln(`<style>${this._cssTextArea.textareaNode.innerText}</style>`)
-    code.writeln(`<script>${this._jsTextArea.textareaNode.innerText}</script>`);
+    code.writeln(`<style>${this.state.css}</style>`)
+    code.writeln(`<script>${this.state.js}</script>`);
     code.close();
   }
 
   updateText(text, type) {
-    switch (type) {
-      case 'html':
-        this.setState({ html: text })
-        break
-      case 'css':
-        this.setState({ css: text })
-        break
-      case 'js':
-        this.setState({ js: text })
-        break
-      default:
-        break
-    }
+    this.setState({ [type]: text })
   }
 
   convert = () => {
@@ -102,9 +149,9 @@ class App extends Component {
         const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
         logger('## decrypted', decryptedData)
-        this.updateText(decryptedData.html ||  defaults.html, 'html')
-        this.updateText(decryptedData.css || defaults.css, 'css')
-        this.updateText(decryptedData.js || defaults.js, 'js')
+        this.updateText(decryptedData.html ||  defaults.html.value, 'html')
+        this.updateText(decryptedData.css || defaults.css.value, 'css')
+        this.updateText(decryptedData.js || defaults.js.value, 'js')
 
         this.compile()
       } else {
@@ -132,36 +179,240 @@ class App extends Component {
     req.send(content);
   }
 
+  getCommandBarItems() {
+      return [
+        {
+          key: 'title',
+          name: 'ETH Sandbox'
+        },
+        {
+          key: 'run',
+          name: 'Run',
+          icon: 'Play',
+          onClick: this.compile,
+        },
+    ]
+  }
+
+  getCommandBarFarItems() {
+    return [
+      {
+        key: 'save',
+        name: 'Save',
+        icon: 'Save',
+        onClick: this.convert,
+      },
+      {
+        key: 'new',
+        name: 'New',
+        icon: 'CircleAddition',
+        onClick: () => window.location.replace(rootAddress),
+      },
+    ]
+  }
+
+  generateButtonColor(color) {
+    return {
+      backgroundColor: color,
+      color: 'white',
+    }
+  }
+
+  generateButtonStyle(rootColor, rootCheckedColor, rootHoverColor, rootCheckedHoverColor) {
+    return {
+        root: this.generateButtonColor(rootColor),
+        rootChecked: this.generateButtonColor(rootCheckedColor),
+        rootHovered: this.generateButtonColor(rootHoverColor),
+        rootCheckedHovered: this.generateButtonColor(rootCheckedHoverColor),
+    }
+  }
+
+  getButtonStyle(key) {
+    const styles = {
+      html: this.generateButtonStyle('#ccc', '#ff8c00'),
+      css: this.generateButtonStyle('#ccc', '#0078d7'),
+      js: this.generateButtonStyle('#ccc', '#ffb900'),
+      output: this.generateButtonStyle('#ccc', '#333333'),
+      console: this.generateButtonStyle('#ccc', '#666666'),
+    }
+    return styles[key]
+  }
+
+  distributePanelsEvenly() {
+    const panelContainerWidth = document.getElementById('panel-container').offsetWidth
+    const visiblePanels = panelOrder.filter(panel => this.state.panels[panel].visible)
+    const panelWidth = panelContainerWidth / visiblePanels.length - DIVIDER_WIDTH
+
+    const updatedPanels = visiblePanels.map(p => ({
+      visible: true,
+      size: panelWidth,
+    })).reduce((acc, cur, i) => {
+      acc[visiblePanels[i]] = cur
+      return acc
+    }, {})
+
+    const panelWidths = []
+    visiblePanels.forEach(panel => {
+      panelWidths.push({ size: panelWidth })
+    })
+
+    this.setState(state => ({
+      panelWidths,
+      panels: {
+        ...state.panels,
+        ...updatedPanels,
+      }
+    }))
+  }
+
+  getEditorButtons = () => {
+    return panelOrder.map(panel => (
+      <DefaultButton
+        key={panel}
+        text={defaults[panel].label}
+        checked={this.state.panels[panel].visible}
+        onClick={() => this.togglePanel(panel)}
+        styles={this.getButtonStyle(panel)}
+      />
+    ))
+  }
+
+  togglePanel = panel => {
+    const panels = panelOrder.reduce((acc, cur) => {
+      acc[cur] = {
+        size: this.state.panels[cur].size,
+        visible: panel === cur ? !this.state.panels[cur].visible : this.state.panels[cur].visible
+      }
+      return acc
+    }, {})
+    this.setState({ panels }, this.distributePanelsEvenly)
+  }
+
+  renderEditor(language, options) {
+    return (
+      <MonacoEditor
+        key={`key-${language}-${Math.floor(this.state.panels[language].size / 50)}`} // Rerender if changed by over 50px
+        value={this.state[language] || ''}
+        onChange={(text) => this.updateText(text, language)}
+        options={options}
+        theme="vs"
+        language={language}
+        width={this.state.panels[language].size}
+      />
+    )
+  }
+
+  renderIFrame() {
+    return (
+      <div
+        key={'key-iframe'}
+        id="iframe-wrapper"
+        style={{ flex: 1, paddingLeft: 20, paddingRight: 20 }}
+      >
+        <iframe
+          ref={i => this._iframe = i}
+          title="Test"
+          id="code"
+          srcDoc="Output"
+          style={{ width: '100%', height: '100%' }}
+          >
+        </iframe>
+    </div>
+    )
+  }
+
+  renderConsole() {
+    return (
+      <div style={{ flex: 1 }} key={'key-console'}>
+        <Consoled
+          consoleLog={(row, i) => <div key={i} style={{padding: 10}}><span className="console-log">{row}</span></div> }
+        >
+          {this.state.consoleJs}
+        </Consoled>
+      </div>
+    )
+  }
+
+  renderPanels(options) {
+    // NOTE: Seeing some buggy behavior when using { this.state.languageVisible && <Component /> } syntax
+    // so I'm explicitly creating the component array instead [dan]
+    const panels = []
+    codeEditors.forEach(language => {
+      if (this.state.panels[language].visible) panels.push(this.renderEditor(language, options))
+    })
+    if (this.state.panels['console'].visible) panels.push(this.renderConsole())
+    if (this.state.panels['output'].visible) panels.push(this.renderIFrame())
+    return panels
+  }
+
+  onUpdatePanel = updatedWidths => {
+    const updatedPanels = panelOrder.filter(p => this.state.panels[p].visible).reduce((acc, cur, i) => {
+      acc[cur] = {
+        visible: true,
+        size: updatedWidths[i].size
+      }
+      return acc
+    }, {})
+    this.setState( state => ({ panelWidths: updatedWidths.map(w => ({ size: w.size })), panels: { ...state.panels, ...updatedPanels } }))
+  }
+
+
   render() {
+    const options =  {
+          selectOnLineNumbers: true,
+          fontSize: 14,
+          automaticLayout: true, // less performant
+          lineNumbers: 'on',
+          theme: 'vs',
+        }
+
     return (
       <div className="App">
-        <div className="app-header">
-          <div className="app-header-left">
-            <span>ETH Sandbox</span>
-            <button onClick={this.compile} id="button">Run</button>
-          </div>
-          <div className="app-header-right">
-            <button onClick={this.convert} id="button">Save</button>
-            <button onClick={() => window.location.replace(rootAddress)} id="button">New</button>
-          </div>
+
+        <div className="command-bar">
+          <CommandBar
+            elipisisAriaLabel='More options'
+            items={this.getCommandBarItems()}
+            farItems={this.getCommandBarFarItems()}
+          />
         </div>
 
         <div className="app-content">
           <div className="app-sidebar">
-            <a href="/b3ecdff9f883d3e98013bebfa364c2a01e62785396464e9e5a8b9cce656dc661">Readme</a>
-            <a href="/0e84c4ed8f22fbb859f4ec4580a1fa731a881940f42e6af1c871a58a746556c3">Examples</a>
+            <Nav
+          groups={
+            [
+              {
+                links:
+                [
+                  { name: 'Playground', url: '/', key: 'key1', onClick: () => this.setState({ selectedKey: 'key1' }) },
+                  { name: 'Readme', url: '/b3ecdff9f883d3e98013bebfa364c2a01e62785396464e9e5a8b9cce656dc661', key: 'key2', onClick: () => this.setState({ selectedKey: 'key2' })},
+                  { name: 'Examples', url: '/0e84c4ed8f22fbb859f4ec4580a1fa731a881940f42e6af1c871a58a746556c3', key: 'key3', onClick: () => this.setState({ selectedKey: 'key3' }) },
+
+                ]
+              }
+            ]
+          }
+          expandedStateText={ 'expanded' }
+          collapsedStateText={ 'collapsed' }
+          selectedKey={ this.state.selectedKey }
+        />
           </div>
-
-
-          <div className="app-main">
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <Codemirror ref={ref => this._htmlTextArea = ref} value={this.state.html || ''} onChange={(text) => this.updateText(text, 'html')} options={{lineNumbers: true, mode: 'htmlmixed', lineWrapping: true }} />
-              <Codemirror ref={ref => this._cssTextArea = ref} value={this.state.css || ''} onChange={(text) => this.updateText(text, 'css')} options={{lineNumbers: true, mode: 'css', lineWrapping: true }} />
+            <div className="app-main">
+              <div style={{ marginBottom: 5}}>
+              <div style={{ display: 'flex', flexDirection: 'row', padding: 5, backgroundColor: '#eaeaea'}}>
+                {this.getEditorButtons()}
+              </div>
             </div>
-
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <Codemirror ref={ref => this._jsTextArea = ref}  value={this.state.js || ''} onChange={(text) => this.updateText(text, 'js')} options={{lineNumbers: true, mode: 'javascript', lineWrapping: true }} />
-              <iframe title="Test" id="code" srcDoc="Output"></iframe>
+            <div id="panel-container" style={{ flex: 1 }}>
+              <PanelGroup
+                borderColor="#DDD"
+                spacing={1}
+                panelWidths={this.state.panelWidths}
+                onUpdate={this.onUpdatePanel}
+              >
+                {this.renderPanels()}
+              </PanelGroup>
             </div>
           </div>
         </div>
---
+++ src/PasteBin.js
@@ -0,0 +1,300 @@
+import React, { Component } from 'react';
+import CryptoJS from 'crypto-js'
+import MonacoEditor from 'react-monaco-editor';
+import PastePanel from './pastebin/PastePanel';
+import SettingsPanel from './pastebin/SettingsPanel';
+import { gethAddress, swarmAddress, rootAddress } from './constants/api'
+
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+import { Dropdown, IDropdown, DropdownMenuItemType, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+
+import { getPasteHash, getPasteKey, generatePasteKey } from './utils/pasteHelper'
+import _ from 'lodash'
+
+import './PasteBin.css';
+
+const VERSION = 1
+
+const PERSIST_DATA = 'blockpaste:persistData'
+const USER_OPTIONS = 'blockpaste:userOptions'
+
+class App extends Component {
+  state = {
+    content: '',
+    description: '',
+    editorMounted: false,
+    filename: '',
+    languages: [],
+    mode: 'plaintext',
+    pastePanelVisible: false,
+    persistOn: true,
+    settingsPanelVisible: false,
+    options: {
+      selectOnLineNumbers: true,
+      fontSize: 14,
+      automaticLayout: true, // less performant
+      lineNumbers: 'on',
+      theme: 'vs',
+    }
+  }
+
+  componentWillMount() {
+    if (localStorage[USER_OPTIONS]) {
+      this.setState({
+        options: JSON.parse(localStorage[USER_OPTIONS])
+      })
+    }
+    this.setState({
+      persistOn: !(localStorage[PERSIST_DATA] === 'false')
+    })
+  }
+
+  componentDidMount() {
+    const pasteHash = getPasteHash()
+    if (getPasteHash()) this.getData(pasteHash)
+  }
+
+  getOverflowItems() {
+      return [
+      {
+        key: 'pastes',
+        name: 'Pastes',
+        icon: 'Paste',
+        onClick: this.showPastes,
+      },
+      {
+        key: 'settings',
+        name: 'Settings',
+        icon: 'Settings',
+        onClick: this.showSettings,
+      },
+    ]
+  }
+
+  getItems() {
+      return [
+      {
+        key: 'new',
+        name: 'New',
+        icon: 'CircleAddition',
+        onClick: () => window.location.replace(rootAddress),
+      },
+      {
+        key: 'save',
+        name: 'Save',
+        icon: 'Save',
+        onClick: this.save,
+      },
+      {
+        key: 'languages',
+        name: 'Syntax',
+        onRender: () => (
+          <div>
+          <Dropdown
+            style={{marginTop: 4, width: '200px', textAlign: 'center'}}
+            options={this.getSelectBoxOptions()}
+            selectedKey={this.state.mode}
+            onChanged={this.handleLanguageChange}
+          />
+        </div>
+        )
+      },
+      this.state.content && {
+        key: 'copy',
+        name: 'Copy to Clipboard',
+        icon: 'Copy',
+        onClick: this.copyToClipboard
+      },
+    ]
+  }
+  copyToClipboard = () => {
+    // Create the textarea input to hold our text.
+    const element = document.createElement('textarea');
+    element.value = this._editor.getValue();
+    // Add it to the document so that it can be focused.
+    document.body.appendChild(element);
+    // Focus on the element so that it can be copied.
+    element.focus();
+    element.setSelectionRange(0, element.value.length);
+    // Execute the copy command.
+    document.execCommand('copy');
+    // Remove the element to keep the document clear.
+    document.body.removeChild(element);
+  }
+
+  showPastes = () => {
+    this.setState({ pastePanelVisible: true })
+  }
+
+  showSettings = () => {
+    this.setState({ settingsPanelVisible: true })
+  }
+
+  save = () => {
+    const { content, description, filename, mode } = this.state
+    if (content.trim().length === 0) return
+    const createdAt = Date.now()
+    const payload = {
+      content,
+      description,
+      filename,
+      mode,
+      // meta data
+      createdAt,
+      schema: VERSION,
+    }
+
+    const key = generatePasteKey()
+    const encryptedPayload = CryptoJS.AES.encrypt(JSON.stringify(payload), key);
+    logger('## encrypted', encryptedPayload.toString())
+
+    const req = new XMLHttpRequest();
+    req.open('POST', `${swarmAddress}/bzzr:/`);
+    req.onload = event => {
+      if (req.status === 200) {
+        if (this.state.persistOn) {
+          localStorage.setItem(`blockpaste:paste:${createdAt}`, JSON.stringify({
+            link: `${req.responseText}#${key}`,
+            createdAt,
+          }));
+        }
+        window.location.replace(`${rootAddress}/${req.responseText}#${key}`)
+      } else {
+        alert(`There was an error saving your snippet'.\nPlease check console logs.`)
+      }
+    }
+    req.send(encryptedPayload);
+  }
+
+  getData = (hash) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', `${swarmAddress}/bzzr:/${hash}`);
+    req.onload = (event) => {
+      if (req.status === 200) {
+        const bytes = CryptoJS.AES.decrypt(req.responseText, getPasteKey());
+        const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        logger('## decrypted', decryptedData)
+
+        const { content, description, filename, mode } = decryptedData
+        this.setState({ content, description, filename, mode })
+      } else {
+        alert(`There was an error accessing path 'bzz:/${hash}'.\nPlease check console logs.`)
+      }
+    }
+    req.send(null);
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleLanguageChange = (event) => {
+    const { key } = event
+    this.setState({ mode: key })
+  }
+
+  editorDidMount = (editor, monaco) => {
+    const languages = _.sortBy(monaco.languages.getLanguages(), 'id')
+
+    this._editor = editor
+    this._editor.focus()
+    this.setState({ languages, editorMounted: true })
+
+  }
+
+  getCurrentLanguage() {
+    return this.state.mode
+  }
+
+  getSelectBoxOptions() {
+    return this.state.languages.map(lang => {
+      const alias = lang.aliases[0] === 'sol' ? 'Solidity' : lang.aliases[0]
+      return { key: lang.id, text: alias }
+    })
+  }
+
+  onChangeTheme = theme => {
+    this.setState(prevState => ({
+      options: {
+        ...prevState.options,
+        theme,
+      }
+    }), this.updateStoredOptions)
+  }
+
+  onChangeLineNubmersOn = lineNumbers => {
+    this.setState(prevState => ({
+      options: {
+        ...this.state.options,
+        lineNumbers,
+      }
+    }), this.updateStoredOptions)
+    }
+
+  onChangeFontSize = fontSize => {
+    this.setState(prevState => ({
+      options: {
+      ...prevState.options,
+      fontSize,
+      }
+    }), this.updateStoredOptions)
+  }
+
+  updateStoredOptions = () => {
+    localStorage.setItem(USER_OPTIONS, JSON.stringify(this.state.options));
+  }
+
+  onPersistChanged = persistOn => {
+    this.setState({ persistOn })
+    localStorage.setItem(PERSIST_DATA, String(persistOn))
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <PastePanel
+          isOpen={this.state.pastePanelVisible}
+          persistOn={this.state.persistOn}
+          onPersistChanged={this.onPersistChanged}
+          onDismiss={() => this.setState({ pastePanelVisible: false })}
+        />
+        <SettingsPanel
+          isOpen={this.state.settingsPanelVisible}
+          onDismiss={() => this.setState({ settingsPanelVisible: false })}
+          theme={this.state.options.theme}
+          onChangeTheme={this.onChangeTheme}
+          fontSize={this.state.options.fontSize}
+          onChangeFontSize={this.onChangeFontSize}
+          lineNumbersOn={this.state.options.lineNumbers}
+          onChangeLineNumbersOn={this.onChangeLineNubmersOn}
+        />
+        <div className="command-bar">
+          <CommandBar
+            elipisisAriaLabel='More options'
+            items={this.getItems()}
+            farItems={ this.getOverflowItems() }
+          />
+        </div>
+        <div className="monaco-editor">
+          <MonacoEditor
+            key={JSON.stringify(this.state.options)}
+            editorDidMount={this.editorDidMount}
+            width="100%"
+            height="100%"
+            language={this.state.editorMounted ? this.state.mode : ''}
+            value={this.state.content}
+            onChange={content => this.setState({ content })}
+            options={this.state.options}
+            theme={this.state.options.theme}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
