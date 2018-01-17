54683e9dc0fcce44e306c484748cc5e5b5d60fcb
Christopher Dro <hello@blick-labs.com>
2018-01-17 11:02:40 -0800

    Run prettier on src'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import PasteBin from './PasteBin';
+import App from "./App";
+import PasteBin from "./PasteBin";
 
 // Register icons and pull the fonts from the default SharePoint cdn.
-import { initializeIcons } from '@uifabric/icons'
-initializeIcons()
+import { initializeIcons } from "@uifabric/icons";
+initializeIcons();
 
 // Create global logger for development
-const logger = (...args) => { if (process.env.NODE_ENV !== 'production') console.log(...args) }
-window.logger = logger
+const logger = (...args) => {
+  if (process.env.NODE_ENV !== "production") console.log(...args);
+};
+window.logger = logger;
 
 // Render main application
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
