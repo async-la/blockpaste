92c32e7719103153f03051a0900f4af983fc9fe0
Christopher Dro <hello@blick-labs.com>
2018-01-17 11:02:40 -0800

    Run prettier on src'

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,12 @@
-export const rootAddress = process.env.NODE_ENV === 'production' ? 'https://blockpaste.com' : 'http://localhost:3000'
-export const gethAddress = process.env.NODE_ENV === 'production' ? 'https://blockpaste.com/geth' : 'http://localhost:8545'
-export const swarmAddress = process.env.NODE_ENV === 'production' ? 'https://blockpaste.com' : 'http://localhost:8500'
+export const rootAddress =
+  process.env.NODE_ENV === "production"
+    ? "https://blockpaste.com"
+    : "http://localhost:3000";
+export const gethAddress =
+  process.env.NODE_ENV === "production"
+    ? "https://blockpaste.com/geth"
+    : "http://localhost:8545";
+export const swarmAddress =
+  process.env.NODE_ENV === "production"
+    ? "https://blockpaste.com"
+    : "http://localhost:8500";
--- src/utils/pasteHelper.js
+++ src/utils/pasteHelper.js
@@ -1,15 +1,20 @@
-import CryptoJS from 'crypto-js'
+import CryptoJS from "crypto-js";
 
 export function getPasteHash(url) {
-  return window.location.pathname.replace(/\/|paste/g, '');
+  return window.location.pathname.replace(/\/|paste/g, "");
 }
 
 export function getPasteKey(url) {
-  return window.location.hash.replace('#', '').replace(/(\?|&).*$/, '');
+  return window.location.hash.replace("#", "").replace(/(\?|&).*$/, "");
 }
 
 export function generatePasteKey() {
-  return window.btoa(String.fromCharCode.apply(null, new Uint8Array(CryptoJS.lib.WordArray.random(128).words)))
+  return window.btoa(
+    String.fromCharCode.apply(
+      null,
+      new Uint8Array(CryptoJS.lib.WordArray.random(128).words)
+    )
+  );
 }
 
 export function encryptPayload(payload, key) {
