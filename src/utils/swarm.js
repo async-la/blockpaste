903c08f6e460590636e4921be098c32740b23fe5
Christopher Dro <hello@blick-labs.com>
2018-01-17 10:22:40 -0800

    [Util] Create helper for Swarm requests

---
+++ src/utils/swarm.js
@@ -0,0 +1,31 @@
+import { swarmAddress } from '../constants/api'
+
+export function BZZRawGetAsync(hash) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', `${swarmAddress}/bzzr:/${hash}`);
+    req.onload = (event) => {
+      if (req.status === 200) {
+        resolve(req.responseText)
+      } else {
+        reject(req.responseText)
+      }
+    }
+    req.send(null);
+  })
+}
+
+export function BZZRawPostAsync(payload) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('POST', `${swarmAddress}/bzzr:/`);
+    req.onload = event => {
+      if (req.status === 200) {
+        resolve(req.responseText)
+      } else {
+        reject(req.responseText)
+      }
+    }
+    req.send(payload);
+  })
+}
