d1ede2f96eb21795baf78e9b1a201028872f7e7e
Jonathan Limtiaco <Jonathanlimtiaco@gmail.com>
2018-01-22 12:47:02 -0800

    Fix copy to clip board

--- src/PasteBin.js
+++ src/PasteBin.js
@@ -13,6 +13,7 @@ import {
   generatePasteKey
 } from "./utils/pasteHelper";
 import { BZZRawGetAsync, BZZRawPostAsync } from "./utils/swarm";
+import { copyToClipboard } from "./utils/copyToClipboard";
 import _ from "lodash";
 
 import "./PasteBin.css";
@@ -114,19 +115,9 @@ class App extends Component {
       }
     ];
   }
+
   copyToClipboard = () => {
-    // Create the textarea input to hold our text.
-    const element = document.createElement("textarea");
-    element.value = this._editor.getValue();
-    // Add it to the document so that it can be focused.
-    document.body.appendChild(element);
-    // Focus on the element so that it can be copied.
-    element.focus();
-    element.setSelectionRange(0, element.value.length);
-    // Execute the copy command.
-    document.execCommand("copy");
-    // Remove the element to keep the document clear.
-    document.body.removeChild(element);
+    copyToClipboard(this._editor.getValue());
   };
 
   showPastes = () => {
