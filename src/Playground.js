c3a32ce275ea8ff109236e26794fbb499cbc39ad
jrlimtiaco <jonathanlimtiaco@gmail.com>
2018-01-23 18:16:03 -0800

    Add input validation for saving (#22)

--- src/Playground.js
+++ src/Playground.js
@@ -34,14 +34,17 @@ const defaults = {
   html: {
     label: 'HTML',
     visible: true,
+    value: '',
   },
   javascript: {
     label: 'Javascript',
     visible: true,
+    value: '',
   },
   css: {
     label: 'CSS',
     visible: true,
+    value: '',
   },
 }
 
@@ -118,6 +121,13 @@ class Playground extends Component {
       createdAt: Date.now(),
       schema: 1,
     }
+
+    if (
+      payload.html.trim().length === 0 &&
+      payload.css.trim().length === 0 &&
+      payload.javascript.trim().length === 0
+    )
+      return
 
     // Encrypt JSON Payload
     const key = generatePasteKey()
