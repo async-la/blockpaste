0eaeff445bf7298739464e6dc245579648fc0c9f
Jonathan Limtiaco <Jonathanlimtiaco@gmail.com>
2018-01-23 18:10:08 -0800

    Remove lodash

--- src/Playground.js
+++ src/Playground.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import _ from 'lodash'
 import BigNumber from 'bignumber.js'
 import InfoPanel from './common/InfoPanel'
 import PanelGroup from 'react-panelgroup'
