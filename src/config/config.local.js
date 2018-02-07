c4cd1677fca0daf969b1dda61cae94d3c509c10b
Christopher Dro <hello@blick-labs.com>
2018-02-07 21:54:41 +0100

    [Config] Remove config.local No longer required since we're inject all require env vars on start/build scripts

--- src/config/config.local.js
+++
@@ -1,5 +0,0 @@
-import { APP_BLOCKPASTE, APP_PLAYGROUND } from '../constants/app'
-
-export default {
-  appId: APP_BLOCKPASTE,
-}
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,20 @@
-import localConfig from './config.local'
-import defaultConfig from './config.default'
+import { APP_BLOCKPASTE, APP_PLAYGROUND } from '../constants/app'
+
+const isBlockpaste = process.env.REACT_APP_ENV === APP_BLOCKPASTE
 
 export default {
-  ...defaultConfig,
-  ...localConfig,
+  appId: isBlockpaste ? APP_BLOCKPASTE : APP_PLAYGROUND,
+  isBlockpaste,
+  rootAddress:
+    process.env.NODE_ENV === 'production'
+      ? isBlockpaste ? 'https://blockpaste.com' : 'https://play.blockpaste.com'
+      : 'http://localhost:3000',
+  gethAddress:
+    process.env.NODE_ENV === 'production'
+      ? 'https://test.bzz.network/geth'
+      : 'https://test.bzz.network/geth',
+  swarmAddress:
+    process.env.NODE_ENV === 'production'
+      ? 'https://test.bzz.network'
+      : 'https://test.bzz.network',
 }
